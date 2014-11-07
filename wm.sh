0e77c129eae0077f6f86cb4a049cb373daaaf8e2
Ammon Shepherd <ammon.shepherd@gmail.com>
2014-11-07 18:15:05 -0500

    put font and label into variables

--- wm.sh
+++ wm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+label='Copyright ©2011 USHMM'
+font='/Users/aes9h/Downloads/fonts/Gudea/Gudea-Regular.ttf' 
 
 if [[ ! -d jpgs ]]; then
     mkdir -p jpgs
@@ -49,24 +51,24 @@ do
         #     $1 \
         #     -background Gold \
         #     -pointsize $s \
-        #     -font /Users/aes9h/Downloads/fonts/Gudea/Gudea-Regular.ttf \
+        #     -font $font \
         #     -gravity south \
         #     -splice 0x$h \
-        #     -annotate +0+22 'Copyright ©2011 USHMM' \
+        #     -annotate +0+22 "$label" \
         #     marked/$1
 
 
         # Overlay white text on dark grey transparent background
-        # convert \
-        #     -background '#00000080' \
-        #     -fill white \
-        #     -size $s \
-        #     -font /Users/aes9h/Downloads/fonts/Gudea/Gudea-Regular.ttf \
-        #     label:'Copyright ©2011 USHMM' \
-        #     miff:- | \
-        #         composite \
-        #         -gravity south \
-        #         -geometry +0+3 \
-        #         -   $1 marked/$1
+        convert \
+            -background '#00000080' \
+            -fill white \
+            -size $s \
+            -font $font \
+            label:"$label" \
+            miff:- | \
+                composite \
+                -gravity south \
+                -geometry +0+3 \
+                -   $1 marked/$1
     fi
 done
